cd46a347cc79a8792161858f8618e4e7d1e6de4f
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-27 04:24:48 -0500

    Randomized platform spawning

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -8,26 +8,44 @@ public class PlatformSpawner : MonoBehaviour {
 
 	Vector3 lastPos;
 	float size;
+	public bool gameOver;
 
 	// Use this for initialization
 	void Start () {
 		lastPos = platform.transform.position;
 		size = platform.transform.localScale.x;
+
+		for (int i = 0; i < 20; i++) {
+			SpawnPlatforms ();
+		}
+
+		InvokeRepeating ("SpawnPlatforms", 2f, 0.2f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (gameOver) {
+			CancelInvoke("SpawnPlatforms");
+		}
 	}
 
-	void SpawnX() {
+	void SpawnPlatforms () {
+		int rand = Random.Range (0, 6);
+		if (rand < 3) {
+			SpawnX ();
+		} else if (rand >= 3) {
+			SpawnZ ();
+		}
+	}
+
+	void SpawnX () {
 		Vector3 pos = lastPos;
 		pos.x += size;
 		lastPos = pos;
 		Instantiate (platform, pos, Quaternion.identity);
 	}
 
-	void SpawnZ() {
+	void SpawnZ () {
 		Vector3 pos = lastPos;
 		pos.z += size;
 		lastPos = pos;
