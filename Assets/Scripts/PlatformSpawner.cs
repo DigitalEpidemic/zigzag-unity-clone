962376b754531a5dd50ec4ca189a41d300168ef4
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-27 04:09:44 -0500

    Setup platform spawner

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+
+	public GameObject platform;
+
+	Vector3 lastPos;
+	float size;
+
+	// Use this for initialization
+	void Start () {
+		lastPos = platform.transform.position;
+		size = platform.transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnX() {
+		Vector3 pos = lastPos;
+		pos.x += size;
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+	}
+
+	void SpawnZ() {
+		Vector3 pos = lastPos;
+		pos.z += size;
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+	}
+}
