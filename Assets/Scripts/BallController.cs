e5d07e5c32de88ea3a733ce6c1dc8f25bde5a881
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-27 02:28:16 -0500

    Added ball falling and removed friction

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,6 +7,8 @@ public class BallController : MonoBehaviour {
 	[SerializeField]
 	private float speed;
 	bool started;
+	bool gameOver;
+
 	Rigidbody rb;
 
 	void Awake () {
@@ -16,6 +18,7 @@ public class BallController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		started = false;
+		gameOver = false;
 	}
 	
 	// Update is called once per frame
@@ -27,7 +30,14 @@ public class BallController : MonoBehaviour {
 			} 
 		}
 
-		if (Input.GetMouseButtonDown (0)) {
+		Debug.DrawRay (transform.position, Vector3.down, Color.red);
+
+		if (!Physics.Raycast (transform.position, Vector3.down, 1f)) {
+			gameOver = true;
+			rb.velocity = new Vector3 (0, -25f, 0);
+		}
+
+		if (Input.GetMouseButtonDown (0) && !gameOver) {
 			SwitchDirection ();
 		}
 	}
