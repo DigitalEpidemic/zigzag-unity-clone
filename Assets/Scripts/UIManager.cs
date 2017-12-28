bd9976a49628922d1d65cde4e85bd5324f9af2fb
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-28 02:17:07 -0500

    Setup UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
+
+	public static UIManager instance;
+
+	public GameObject zigZagPanel;
+	public GameObject gameOverPanel;
+	public GameObject tapText;
+	public Text score;
+	public Text highScore1;
+	public Text highScore2;
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
 		
+	}
+
+	public void GameStart () {
+		tapText.SetActive (false);
+		zigZagPanel.GetComponent<Animator> ().Play ("PanelUp");
+	}
+
+	public void GameOver () {
+		gameOverPanel.SetActive (true);
+	}
+
+	public void Reset () {
+		SceneManager.LoadScene (0);
 	}
 	
 	// Update is called once per frame
